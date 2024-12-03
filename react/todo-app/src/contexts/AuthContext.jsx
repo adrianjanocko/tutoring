59bde78e9ced1a0739f9606d1c232eca982bb31d
Adrián Janočko <adonikeoffice@gmail.com>
2024-12-03 18:38:10 +0100

    ➖ No need to set session 2x.

--- react/todo-app/src/contexts/AuthContext.jsx
+++ react/todo-app/src/contexts/AuthContext.jsx
@@ -8,11 +8,6 @@ export default function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setUser(session?.user ?? null);
-      setLoading(false);
-    });
-
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
